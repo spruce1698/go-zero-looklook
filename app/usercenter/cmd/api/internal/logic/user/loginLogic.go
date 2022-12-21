654ddec8620fd832ca0361502a5d2cb542eba0dc
spruce <99873080+spruce1698@users.noreply.github.com>
2022-12-21 16:08:14 +0800

    优化目录

--- app/usercenter/cmd/api/internal/logic/user/loginLogic.go
+++ app/usercenter/cmd/api/internal/logic/user/loginLogic.go
@@ -1,43 +1,45 @@
 package user
 
 import (
-	"context"
-
-	"looklook/app/usercenter/cmd/api/internal/svc"
-	"looklook/app/usercenter/cmd/api/internal/types"
-	"looklook/app/usercenter/cmd/rpc/usercenter"
-	"looklook/app/usercenter/model"
-
-	"github.com/jinzhu/copier"
-	"github.com/zeromicro/go-zero/core/logx"
+    "context"
+    
+    "looklook/app/usercenter/cmd/api/internal/svc"
+    "looklook/app/usercenter/cmd/api/internal/types"
+    "looklook/app/usercenter/cmd/rpc/usercenter"
+    "looklook/app/usercenter/model"
+    
+    "github.com/jinzhu/copier"
+    "github.com/zeromicro/go-zero/core/logx"
 )
 
 type LoginLogic struct {
-	logx.Logger
-	ctx    context.Context
-	svcCtx *svc.ServiceContext
+    logx.Logger
+    ctx    context.Context
+    svcCtx *svc.ServiceContext
 }
 
 func NewLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) LoginLogic {
-	return LoginLogic{
-		Logger: logx.WithContext(ctx),
-		ctx:    ctx,
-		svcCtx: svcCtx,
-	}
+    return LoginLogic{
+        Logger: logx.WithContext(ctx),
+        ctx:    ctx,
+        svcCtx: svcCtx,
+    }
 }
 
 func (l *LoginLogic) Login(req types.LoginReq) (*types.LoginResp, error) {
-	loginResp, err := l.svcCtx.UsercenterRpc.Login(l.ctx, &usercenter.LoginReq{
-		AuthType: model.UserAuthTypeSystem,
-		AuthKey:  req.Mobile,
-		Password: req.Password,
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	var resp types.LoginResp
-	_ = copier.Copy(&resp, loginResp)
-
-	return &resp, nil
+    logx.Error("测试的日志")
+    
+    loginResp, err := l.svcCtx.UsercenterRpc.Login(l.ctx, &usercenter.LoginReq{
+        AuthType: model.UserAuthTypeSystem,
+        AuthKey:  req.Mobile,
+        Password: req.Password,
+    })
+    if err != nil {
+        return nil, err
+    }
+    
+    var resp types.LoginResp
+    _ = copier.Copy(&resp, loginResp)
+    
+    return &resp, nil
 }
